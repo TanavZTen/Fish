727d8aeaf8123d1d9f948f1dcd4fb1886292e85b
TanavZTen <savagetanav@gmail.com>
2026-01-03 14:21:24 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+// Firebase Configuration
+// Get your values from Firebase Console: Project Settings > Web App
+// Keep this file out of version control (add to .gitignore)
+
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAne3wIkcjTR7YGwtQH2Si8czbqbOvnGEY",
+  authDomain: "fish-368fe.firebaseapp.com",
+  databaseURL: "https://fish-368fe-default-rtdb.firebaseio.com",
+  projectId: "fish-368fe",
+  storageBucket: "fish-368fe.firebasestorage.app",
+  messagingSenderId: "421214386924",
+  appId: "1:421214386924:web:a6157129cfcc85d7a743c9"
+};
